bb358a05a699810963463cfbee77096e3dc8d089
Inacio Ferrarini <inacio.ferrarini@accenture.com>
2019-07-04 15:49:48 -0300

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Inácio Ferrarini on 04/07/19.
+//  Copyright © 2019 inacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
